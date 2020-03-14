7b682a4473fbaba552f607143da98c7e17b45fa9
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-03-14 21:44:52 +0000

    Num primo - C

---
+++ Anyway/1165.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main()
+{
+    int q, i;
+    int num, j, contDiv;
+    scanf("%d", &q);
+    for (i = 0; i < q; i++)
+    {
+        contDiv = 0;
+        scanf("%d", &num);
+        for (j = 1; j <= num; j++)
+        {
+            if (num % j == 0)
+            {
+                contDiv++;
+            }
+            if (contDiv > 2)
+            {
+                break;
+            }
+        }
+        if (contDiv == 2)
+        {
+            printf("%d eh primo\n", num);
+        }
+        else
+        {
+            printf("%d nao eh primo\n", num);
+        }
+    }
+}
