4c39dd90fe36a7e735cdfe199ea80a3f363d709b
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-03-21 22:56:58 +0000

    Etapa 2 - Strings/Listas

---
+++ TheHuxley/Etapas/MensagemSecreta.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+for i in range(n):
+    pa = str(input())
+    nova = ""
+    cont = 0
+    for k in range(len(pa)):
+        if(pa[k] != " " and cont==0):
+            nova += pa[k]
+            cont=1
+        elif(pa[k-1] == " "):
+            cont=0
+        elif(pa[k-1] == " " and pa[k-2] == " "):
+            cont=0
+            if(pa[k] == " " and pa[k+1] == " "):
+                cont=0
+            elif(pa[k] == " "):
+                cont=0
+            if(pa[k] == " " and pa[k+1] == " "):
+                cont=0
+            elif(pa[k] == " "):
+                cont=0
+        elif(pa[k] == " "):
+            cont=0
+        
+    print(nova)
