1699b2514286b63ecd9b1bf6c87581d41475f223
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-04-22 21:30:50 +0000

    Mensagem Oculta

---
+++ URI/1272.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int i, n, k, ind, len1;
+    scanf("%d", &n);
+    getchar();
+    for (i = 0; i < n; i++)
+    {
+        char pa[51], nova[51];
+        ind = 0;
+        gets(pa);
+        len1 = strlen(pa);
+        for (k = 0; k < len1; k++)
+        {
+            if(k == 0 && pa[0] != ' '){
+                nova[ind] = pa[0];
+                ind++;
+            }
+            if(pa[k] == ' ' && pa[k+1] == ' '){
+                continue;
+            } 
+            if(pa[k] == ' ' && pa[k+1] != ' '){
+                nova[ind] = pa[k+1];
+                ind++;
+            }
+        }
+        nova[ind] = '\0';
+        puts(nova);
+    }
+    return 0;
+}
