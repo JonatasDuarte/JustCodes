29a926460399ca0221b9b908243e564d89a7ce32
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-04-25 02:38:54 +0000

    pa

---
+++ TheHuxley/Roteiro 4 - Strings/Palindromo.py
@@ -0,0 +1,11 @@
+n = int(input())
+novo = ""
+for i in range(n):
+    frase = str(input())
+    frase = frase.replace(" ","")
+    frase = frase.upper()
+    novo = frase[::-1]
+    if(frase == novo):
+        print("SIM")
+    else:
+        print("NAO")
