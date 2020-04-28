b664ddf105db392618f1b31c0607e352aaac4d0d
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-04-28 02:16:30 +0000

    OBI URI

---
+++ URI/2062.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    int n, i, j, tam;
+    scanf("%d", &n);
+    char pa[9999];
+
+    for (i = 0; i < n; i++){
+        scanf("%s", pa);
+        tam = strlen(pa);
+        for(j=0; j<tam-1; j++){
+            toupper(pa[j]);
+        }
+        if (tam == 3)
+        {
+            if (pa[0] == 'U' && pa[1] == 'R')
+            {
+                printf("URI");
+            }
+            else if (pa[0] == 'O' && pa[1] == 'B')
+            {
+                printf("OBI");
+            }
+            else
+            {
+                printf("%s", pa);
+            }
+        }
+        else
+        {
+            printf("%s", pa);
+        }
+        if (i < n - 1){
+            printf(" ");
+        }
+    }
+    printf("\n");
+    return 0;
+}
