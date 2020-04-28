ed3cce466801c43be59a9ac5084a673cd317b5f1
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-04-28 02:17:21 +0000

    up

---
+++ URI/2248.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+    int n, i;
+    int cont= 0;
+    scanf("%d", &n);
+    while(n != 0){
+        int notas[n], id[n], maior;
+        maior = 0;
+        cont ++;
+        for (i =0; i<n; i++){
+            scanf("%d %d", &id[i], &notas[i]);
+            if(notas[i] >= maior){
+                maior = notas[i];
+            }
+        }
+        printf("Turma %d\n", cont);
+        for(i=0; i<n; i++){
+            if(notas[i] == maior){
+                printf("%d ", id[i]);
+            }
+        } 
+        
+        printf("\n\n");
+        scanf("%d", &n);
+    }
+    return 0;
+}
