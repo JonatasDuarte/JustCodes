2bc143386da32948a7838b00b81a8a91c155a37c
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-04-28 02:09:18 +0000

    Borboletas

---
+++ URI/1901.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, ero, um, j, k, dos;
+    scanf("%d", &n);
+    int res;
+    int floresta[n][n];
+    ero = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &floresta[i][j]);
+        }
+    }
+    for (j = 0; j < (2 * n); j++)
+    {
+        scanf("%d %d", &um, &dos);
+        res = floresta[um - 1][dos - 1];
+        if (res != 0)
+        {
+            ero++;
+            for (j = 0; j < n; j++)
+            {
+                for (k = 0; k < n; k++)
+                {
+                    if (floresta[j][k] == res)
+                    {
+                        floresta[j][k] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("%d\n", ero);
+    return 0;
+}
