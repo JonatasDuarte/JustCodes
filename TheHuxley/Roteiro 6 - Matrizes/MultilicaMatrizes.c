d67184946acce01d2a9292bfb9b4a71bce0b5a53
Jonatas Duarte <jonatas.silva@academico.ifpb.edu.br>
2020-07-04 21:36:22 +0000

    Multiplicação de Matrizes

---
+++ TheHuxley/Roteiro 6 - Matrizes/MultilicaMatrizes.c
@@ -0,0 +1,20 @@
+num = input().split()
+n = int(num[0])
+m = int(num[1])
+o = int(num[2])
+a = []
+b = []
+c = [[0 for i in range(n)]for k in range(o)]
+
+for i in range(n):
+    a.append([int(j) for j in str(input()).split()])
+for j in range(m):
+    b.append([int(i) for i in str(input()).split()])
+
+for k in range(n):
+    for j in range(o):
+        for i in range(m):
+            c[k][j] += a[k][i] * b[i][j]
+
+for x in c:
+    print(*x, sep=" ")
